b309730dcb322b6efba531351756dd04a22803e7
aloksawant <alok.sawant@gmail.com>
2014-04-16 23:48:52 -0700

    Fixed MoveTo behavior bug. Added GroupMoveTo, CheckCriticalTarget, FindGroupMoveToEnemy, FindCenterAttack, FindCriticalTarget
    Added Global Tactical Behavior that checks for critical target. Also uses global blackboard.
    
    Added Zealot group and behavior.

--- ExampleAIModule/Source/ActionBehaviors.h
+++ ExampleAIModule/Source/ActionBehaviors.h
@@ -14,6 +14,17 @@ private:
 	bool					m_moveAttack;
 };
 
+class GroupMoveTo : public Behavior
+{
+public:
+
+	virtual void			OnInitialize();
+	virtual BH_STATUS		Update(float deltaTime);
+
+private:
+	bool					m_moveAttack;
+};
+
 class Attack : public Behavior
 {
 public:
@@ -38,3 +49,30 @@ class CalculateFallbackPos : public Behavior
 public:
 	virtual BH_STATUS		Update(float deltaTime);
 };
+
+// Melee - Zealots
+class CheckCriticalTarget : public Behavior
+{
+public:
+	virtual BH_STATUS		Update(float deltaTime);
+};
+
+class FindGroupMoveToEnemy : public Behavior
+{
+public:
+	virtual BH_STATUS		Update(float deltaTime);
+};
+
+class FindCenterAttack : public Behavior
+{
+public:
+	virtual BH_STATUS		Update(float deltaTime);
+};
+
+// Global
+class FindCriticalTarget : public Behavior
+{
+public:
+	virtual BH_STATUS		Update(float deltaTime);
+};
+
